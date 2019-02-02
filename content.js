7017c73cbd8ba325274145e237e0e5fbcff9b7db
Sriram Ganesan <Sriram@rippling.com>
2019-02-02 00:23:29 -0800

    Initial commit

---
+++ content.js
@@ -0,0 +1,62 @@
+// Utility for writing to the clipboard
+const writeClipboard = (str) => {
+	var copyFrom = document.createElement("textarea");
+	copyFrom.textContent = str;
+	document.body.appendChild(copyFrom);
+	copyFrom.select();
+	document.execCommand('copy');
+	document.body.removeChild(copyFrom);
+}
+
+// Initialize the InboxSDK
+InboxSDK.load('1', 'mail_linker').then(function(sdk){
+	var barMessage;
+
+	// Whenever a message is loaded, add a menu button to the More menu
+	sdk.Conversations.registerMessageViewHandler(function(messageView) {
+		messageView.addToolbarButton({
+			section: 'MORE',
+			title: 'Copy Email PermaLink',
+			onClick: function () {
+
+				messageView.getMessageIDAsync().then(function (messageId) {
+					// When it's clicked, send a message to page.js
+					barMessage = sdk.ButterBar.showSaving({text: "Loading...", confirmationText: "Message link copied to clipboard!"});
+					window.postMessage({type: 'FETCH_ATTACHMENT', id: messageId}, "*");
+				});
+			},
+			orderHint: 0
+		});
+	});
+
+	// Handlers for message id success and failure
+	window.addEventListener("message", function(event) {
+		if (event.source != window || event.data.type != 'FETCH_RESULT') {
+			return;
+		}
+
+		writeClipboard('https://mail.google.com/mail/u/0/#search/rfc822msgid:' + event.data.result);
+		barMessage.resolve();
+	});
+
+	window.addEventListener("message", function(event) {
+		if (event.source != window || event.data.type != 'FETCH_ERROR') {
+			return;
+		}
+
+		barMessage.reject();
+		sdk.ButterBar.showError({text: "Unable to find message ID"});
+	});
+});
+
+/**
+ * Inject the page script.
+ *
+ * See page.js for why we need to do this
+ */
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('page.js');
+s.onload = function () {
+	this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(s);
